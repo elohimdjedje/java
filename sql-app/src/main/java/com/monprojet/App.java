b732747733241c87ad9eb5e29606fe7a1ae377f6
elohimdjedje <mickaeldjedje7@gmail.com>
2025-02-18 16:14:01 +0100

    message

---
+++ sql-app/src/main/java/com/monprojet/App.java
@@ -0,0 +1,36 @@
+package com.monprojet;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String url = "jdbc:mysql://localhost:3306/mabase"; // Remplacer "maBase" par le nom de votre base
+        String utilisateur = "root";
+        String motDePasse = "";
+        Connection connexion = null;
+
+        System.out.println( "Hello World!" );
+        try {
+            // Établir la connexion
+            connexion = DriverManager.getConnection(url, utilisateur, motDePasse);
+            System.out.println("Connexion réussie !");
+            
+        } catch (SQLException e) {
+            System.out.println("Erreur de connexion : " + e.getMessage());
+        } finally { // Toujours fermer la connexion pour éviter les fuites de ressources 
+	        if (connexion != null) { 
+		        try { 
+			        connexion.close(); 
+			        System.out.println("Connexion fermée avec succès."); 
+			    } catch (SQLException e) { 
+				    System.err.println("Erreur lors de la fermeture de la connexion : " + e.getMessage()); 
+				} 
+			} 
+		}
+        
+    }
+}
